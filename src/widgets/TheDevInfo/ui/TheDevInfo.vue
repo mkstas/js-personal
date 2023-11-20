c28c3da5efd00a6ffe5f2cdf7ba5fcaffb7b7773
mkstas <makarov.stn@gmail.com>
2023-11-20 18:02:44 +0300

    refactor: change margins

--- src/widgets/TheDevInfo/ui/TheDevInfo.vue
+++ src/widgets/TheDevInfo/ui/TheDevInfo.vue
@@ -6,17 +6,17 @@ import { developer } from '@/entities/developer';
 
 <template>
   <section class="py-10">
-    <h1 class="text-4xl font-semibold" v-if="developer.name">
+    <h1 class="mb-1 text-4xl font-semibold" v-if="developer.name">
       {{ developer.name }}
     </h1>
     <p
-      class="mt-1 text-xl font-medium text-slate-500"
+      class="mb-12 text-xl font-medium text-slate-500"
       v-if="developer.specialization"
     >
       {{ developer.specialization }}
     </p>
     <a
-      class="flex items-center gap-2 mt-14 hover:underline"
+      class="flex items-center gap-2 hover:underline"
       v-if="developer.githubUrl"
       :href="developer.githubUrl"
       target="_blank"
