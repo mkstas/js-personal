93daa780bde4dd0c953520821f437d5d7aa3afa9
mkstas <makarov.stn@gmail.com>
2024-01-11 11:06:06 +0300

    change skills

--- src/entities/skill/model/index.ts
+++ src/entities/skill/model/index.ts
@@ -1,15 +1,13 @@
 import { IconEnum } from '@/shared/typicode';
 
 export const skills: IconEnum[] = [
-  IconEnum.html,
-  IconEnum.css,
-  IconEnum.sass,
-  IconEnum.tailwind,
   IconEnum.javaScript,
   IconEnum.typeScript,
   IconEnum.vue,
-  IconEnum.react,
   IconEnum.vite,
+  IconEnum.laravel,
+  IconEnum.sass,
+  IconEnum.tailwind,
   IconEnum.git,
   IconEnum.firebase,
 ];
